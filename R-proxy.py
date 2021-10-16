dd3b9cda330ddc9799f19ee6217197e0cb7c149e
RemaxBoxTeam <90772703+RemaxBoxTeam@users.noreply.github.com>
2021-10-16 03:34:14 -0700

    Update R-proxy.py

--- R-proxy.py
+++ R-proxy.py
@@ -42,23 +42,73 @@ def socks5():
 def http():
     f =  requests.get("http://pubproxy.com/api/proxy?limit=1&format=txt&type=http").text
     return f 
+def http_1():
+    site = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all"
+    fsa = requests.get(site)
+    with open("proxy.txt","wb") as test:
+        test.write(fsa.content)
+def socks4_1():
+    site = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all"
+    fsa = requests.get(site)
+    with open("proxy.txt","wb") as test:
+        test.write(fsa.content)
+def socks5_1():
+    site = "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all"
+    fsa = requests.get(site)
+    with open("proxy.txt","wb") as test:
+        test.write(fsa.content)
 test  = int(input(fs + "\n1-socks4\n2-socks5\n3-http" + gn + "\nPlease Choose an option that you want -> " + cl))
 
 if test == 1:
     print (yl + "You chose First option ..")
     print (rd + "Please wait ...")
-    time.sleep(2)
-    print(gn + "\n\n The operation has been successfully \n \n" +  bl + "Proxy : "+str(socks4()) + cl)
-    
+    opr = input(gn  + "1) Single Proxy\n2) Proxy list\nSelect an option ->")
+    if opr == "1":
+
+        print (yl + "You chose First option ..")
+        print (rd + "Please wait ...")
+        time.sleep(2)
+        print(gn + "\n\n The operation has been successfully \n \n" +  bl + "Proxy : "+str(socks4()) + cl)
+    elif opr == "2":
+        print (yl + "You chose second option ..")
+        print (rd + "Please wait ...")
+        socks4_1()
+        print (rd + "done , Created Proxy.txt , check the file " + cl)
+    else:
+        print ("invalid option :)" + cl)
+
 elif test == 2:
     print (yl + "You chose First option ..")
     print (rd + "Please wait ...")
-    time.sleep(2)
-    print(gn + "\n\n The operation has been successfully \n \n" +  bl + "Proxy : "+str(socks5()) + cl)
-    
+    opr = input(gn  + "1) Single Proxy\n2) Proxy list\nSelect an option ->")
+    if opr == "1":
+
+        print (yl + "You chose First option ..")
+        print (rd + "Please wait ...")
+        time.sleep(2)
+        print(gn + "\n\n The operation has been successfully \n \n" +  bl + "Proxy : "+str(socks5()) + cl)
+    elif opr == "2":
+        print (yl + "You chose second option ..")
+        print (rd + "Please wait ...")
+        socks5_1()
+        print (rd + "done , Created Proxy.txt , check the file " + cl)
+    else:
+        print ("invalid option :)" + cl)
+
 elif test == 3:
     print (yl + "You chose First option ..")
     print (rd + "Please wait ...")
-    time.sleep(2)
-    print(gn + "\n\nThe operation has been successfully \n \n" +  bl + "Proxy : "+str(http()) + cl)
-    
+    opr = input(gn  + "1) Single Proxy\n2) Proxy list\nSelect an option ->")
+    if opr == "1":
+
+        print (yl + "You chose First option ..")
+        print (rd + "Please wait ...")
+        time.sleep(2)
+        print(gn + "\n\n The operation has been successfully \n \n" +  bl + "Proxy : "+str(http()) + cl)
+    elif opr == "2":
+        print (yl + "You chose second option ..")
+        print (rd + "Please wait ...")
+        http_1()
+        print (rd + "done , Created Proxy.txt , check the file " + cl)
+    else:
+        print ("invalid option :)" + cl)
